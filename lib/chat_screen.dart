f462f5d7c9aaebc335380afcc88245ca934c2f76
Burhanuddin Rashid <burhanrashid5253@gmail.com>
2018-04-18 16:41:10 +0530

    List Preview

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -1,19 +1,64 @@
 import 'package:flutter/material.dart';
+import 'dummy_data.dart';
 
 class ChatScreen extends StatelessWidget {
+  var chatList = getDummyData();
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
+        floatingActionButton: new FloatingActionButton(
+            backgroundColor: Colors.green,
+            child: new Icon(
+              Icons.message,
+              color: Colors.white,
+            ),
+            onPressed: null),
         body: new ListView.builder(
-      itemBuilder: (BuildContext context, int index) => new Text("Number"),
-      itemCount: 100,
-    ));
+          itemBuilder: (BuildContext context, int index) =>
+              new ChatItem(chatList[index]),
+          itemCount: chatList.length,
+        ));
   }
 }
 
-class ChatModel {
-  final String name;
-  final String message;
-  final String avatarUrl;
-  final String time;
+class ChatItem extends StatelessWidget {
+  final ChatModel _chatModel;
+
+  ChatItem(this._chatModel);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Column(
+      children: <Widget>[
+        new ListTile(
+          leading: new CircleAvatar(
+            backgroundImage: new NetworkImage(_chatModel.avatarUrl),
+          ),
+          title: new Padding(
+            padding: const EdgeInsets.all(3.0),
+            child: new Text(
+              _chatModel.name,
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+              style: new TextStyle(fontWeight: FontWeight.bold, fontSize: 18.0),
+            ),
+          ),
+          subtitle: new Padding(
+            padding: const EdgeInsets.all(3.0),
+            child: new Text(
+              _chatModel.message,
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+              style: new TextStyle(fontSize: 16.0),
+            ),
+          ),
+          trailing: new Text(_chatModel.time),
+        ),
+        new Divider(
+          color: Colors.grey,
+        )
+      ],
+    );
+  }
 }
