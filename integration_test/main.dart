7efeac11fdce4535e9e3a1dfd64ffd257bbc74b6
Burhanuddin Rashid <burhanrashid5253@gmail.com>
2021-03-27 08:32:11 +0530

    Optimize import

--- integration_test/main.dart
+++ integration_test/main.dart
@@ -1,4 +1,3 @@
-import 'package:integration_test/integration_test.dart';
 import 'about_us_page_test.dart' as about;
 import 'add_label_page_test.dart' as label;
 import 'add_project_page_test.dart' as project;
@@ -7,7 +6,7 @@ import 'completed_tasks_page_test.dart' as tasks_completed;
 import 'home_page_test.dart' as home;
 
 void main() {
-  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+  //Run test
   home.main();
   tasks.main();
   tasks_completed.main();
